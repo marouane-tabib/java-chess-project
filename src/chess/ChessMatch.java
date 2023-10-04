835f58550c1222aca3b68c9a43cfc7302261fa31
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-04 11:02:16 +0100

    Add getPieces method with ChessPiece matrix type, use it to generate the pieces in board - ChessMatch.java

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,4 +8,16 @@ public class ChessMatch {
     public ChessMatch() {
         this.board = new Board(8, 8);
     }
+
+    public ChessPiece[][] getPieces() {
+        ChessPiece[][] matrix = new ChessPiece[this.board.getRows()][this.board.getColumns()];
+
+        for (int i = 0; i < this.board.getRows(); i++) {
+            for (int j = 0; j < this.board.getColumns(); j++) {
+                matrix[i][j] = (ChessPiece) this.board.piece(i, j);
+            }
+        }
+
+        return matrix;
+    }
 }
