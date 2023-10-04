f281a310a0d41900cf568ca9bfcb46a614bcfa48
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-04 09:29:42 +0100

    Add private int row|column attributes and construct to inject this attributes - Position.java

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,4 +1,11 @@
 package boardgame;
 
 public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
 }
