265939bfe44ef7982310d2d87bbc221fee8429aa
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-04 16:33:24 +0100

    Add toPosition method to new position of the piece - chess/ChessPosition

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import boardgame.Position;
+
 public class ChessPosition {
 
     private char column;
@@ -21,4 +23,16 @@ public class ChessPosition {
         return row;
     }
 
+    protected Position toPosition() {
+        return new Position(8 - row, column - 'a');
+    }
+
+    protected static ChessPosition fromPosition(Position position) {
+        return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+    }
+
+    @Override
+    public String toString() {
+        return "" + column + row;
+    }
 }
