7550b6f887238dfe7355753b3cd2252b4799fa67
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-04 19:13:58 +0100

    Add while loop with console message to get the source|target from the console inputs - app/app.java

--- src/app/app.java
+++ src/app/app.java
@@ -1,12 +1,30 @@
 package app;
 
 import  chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class app {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        ConsoleInterface.printBoard(chessMatch.getPieces());
+
+        while (true) {
+            ConsoleInterface.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = ConsoleInterface.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = ConsoleInterface.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 
 }
