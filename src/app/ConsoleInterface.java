fee2231aa261041d0bc00a131e1662858d427be5
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-04 11:09:45 +0100

    Add printPiece to check if we have piece object value or null type, to choice what the we need to print it - or piece object - ConsoleInterface.java

--- src/app/ConsoleInterface.java
+++ src/app/ConsoleInterface.java
@@ -1,5 +1,15 @@
 package app;
 
+import chess.ChessPiece;
+
 public class ConsoleInterface {
 
+    private static void printPiece(ChessPiece piece) {
+        if (piece == null) {
+            System.out.print("-");
+        } else {
+            System.out.print(piece);
+        }
+        System.out.print(" ");
+    }
 }
