07cd7e3c0dd574e42a6ad7b49d8d3f0fe7734a15
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-04 16:52:05 +0100

    Add placeNewPiece method to change place of the piece with column char and row int - chess/ChessMatch.java

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,8 +26,12 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        board.placePiece(new Rook(this.board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(this.board, Color.BLACK), new Position(0, 4));
-        board.placePiece(new King(this.board, Color.WHITE), new Position(7, 4));
+        placeNewPiece('b', 6, new Rook(this.board, Color.WHITE));
+        placeNewPiece('e', 8, new King(this.board, Color.BLACK));
+        placeNewPiece('e', 1, new King(this.board, Color.WHITE));
+    }
+
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
 }
