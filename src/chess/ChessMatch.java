63c4d5c6b984731ac91c70e924016c55d857694f
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-04 14:52:23 +0100

    Add initialSetup method to put the chess pieces and thier positions on the board, to preparing chess match to run up - chess/ChessMatch.java

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
@@ -19,5 +22,11 @@ public class ChessMatch {
         }
 
         return matrix;
+    }
+
+    private void initialSetup() {
+        board.placePiece(new Rook(this.board, Color.WHITE), new Position(2, 1));
+        board.placePiece(new King(this.board, Color.BLACK), new Position(0, 4));
+        board.placePiece(new King(this.board, Color.WHITE), new Position(7, 4));
     }
 }
