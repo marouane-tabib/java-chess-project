8467f572edead3850d512a9fd70a85991cb1fe3e
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-04 09:43:40 +0100

    Add the attributes private position with Postion Object type, and board with Board Object type, And construct to inject this attributes - Position.java

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,4 +1,11 @@
 package boardgame;
 
 public class Piece {
+    protected Position position;
+    protected Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        this.position = null;
+    }
 }
