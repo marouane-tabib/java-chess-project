f726f2ed0568ab5fff99dbace169b7c02c96f5ac
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-04 15:12:40 +0100

    Add condition to check if rows|columns values < 1, before we run any method - boardgame/Board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+        }
         this.rows = rows;
         this.columns = columns;
         this.pieces = new Piece[rows][columns];
