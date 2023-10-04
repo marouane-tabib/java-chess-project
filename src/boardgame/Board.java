a0ced6ee63861a70609df91c3279006d7ef44e51
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-04 10:18:19 +0100

    Add getters and setters of row|column attributes, and piece method with other overloading it - Position.java

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -9,6 +9,29 @@ public class Board {
         this.rows = rows;
         this.columns = columns;
         this.pieces = new Piece[rows][columns];
+    }
 
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    public Piece piece(int row, int column) {
+        return this.pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return this.pieces[position.getRow()][position.getColumn()];
     }
 }
