2b5ba2a299048b7cebb9efd56c22aa51b05e063c
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-05 16:56:10 +0100

    Add Overiding printBoard method - ConsoleInterface.java

--- src/app/ConsoleInterface.java
+++ src/app/ConsoleInterface.java
@@ -30,18 +30,31 @@ public class ConsoleInterface {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
-    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
-        System.out.println(ANSI_CYAN + "   _________________" + ANSI_RESET);
-        for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8 - i) + ANSI_CYAN + " | " + ANSI_RESET);
-            for (int j = 0; j < pieces[i].length; j++) {
-                printPiece(pieces[i][j], possibleMoves[i][j]);
-            }
-            System.out.println(ANSI_CYAN + "|" + ANSI_RESET);
-        }
-        System.out.println(ANSI_CYAN + "   _________________" + ANSI_RESET);
-        System.out.println("    a b c d e f g h");
-    }
+	public static void printBoard(ChessPiece[][] pieces) {
+		System.out.println(ANSI_CYAN + "   _________________" + ANSI_RESET);
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + ANSI_CYAN + " | " + ANSI_RESET);
+			for (int j = 0; j < pieces[i].length; j++) {
+				printPiece(pieces[i][j], false);
+			}
+			System.out.println(ANSI_CYAN + "|" + ANSI_RESET);
+		}
+		System.out.println(ANSI_CYAN + "   _________________" + ANSI_RESET);
+		System.out.println("    a b c d e f g h");
+	}
+
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		System.out.println(ANSI_CYAN + "   _________________" + ANSI_RESET);
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + ANSI_CYAN + " | " + ANSI_RESET);
+			for (int j = 0; j < pieces[i].length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
+			}
+			System.out.println(ANSI_CYAN + "|" + ANSI_RESET);
+		}
+		System.out.println(ANSI_CYAN + "   _________________" + ANSI_RESET);
+		System.out.println("    a b c d e f g h");
+	}
 
     private static void printPiece(ChessPiece piece, boolean background) {
         if (background) {
