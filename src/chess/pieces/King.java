52d99ab5735a0975e96ae24e06e0d530b7f18de5
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-05 14:42:43 +0100

    Add possible moves of king piese - chess/pieces/King.java

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -17,8 +17,60 @@ public class King extends ChessPiece {
     }
 
     @Override
+
     public boolean[][] possibleMoves() {
         boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+        // left
+        p.setValues(getPosition().getRow(), getPosition().getColumn() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
+        // left+up
+        p.setValues(getPosition().getRow() - 1, getPosition().getColumn() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
+        // up
+        p.setValues(getPosition().getRow() - 1, getPosition().getColumn());
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
+        // up+right
+        p.setValues(getPosition().getRow() - 1, getPosition().getColumn() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
+        // right
+        p.setValues(getPosition().getRow(), getPosition().getColumn() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
+        // right+down
+        p.setValues(getPosition().getRow() + 1, getPosition().getColumn() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
+        // down
+        p.setValues(getPosition().getRow() + 1, getPosition().getColumn());
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
+        // down+left
+        p.setValues(getPosition().getRow() + 1, getPosition().getColumn() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            matrix[p.getRow()][p.getColumn()] = true;
+        }
+
         return matrix;
     }
 
