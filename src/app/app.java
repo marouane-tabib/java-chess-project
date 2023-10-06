0f803806aa59fb9c504eacca6734390c343276a7
marwan-tabib <marwanmaroc297@gmail.com>
2023-10-06 10:55:30 +0100

    Add captured variable, and inject it to printMatch method - app/app.java

--- src/app/app.java
+++ src/app/app.java
@@ -5,7 +5,9 @@ import  chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class app {
@@ -14,11 +16,12 @@ public class app {
 
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
+        List<ChessPiece> captured = new ArrayList<>();
 
         while (true) {
             try {
                 ConsoleInterface.clearScreen();
-                ConsoleInterface.printMatch(chessMatch.getPieces());
+                ConsoleInterface.printMatch(chessMatch, captured);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = ConsoleInterface.readChessPosition(sc);
@@ -33,6 +36,9 @@ public class app {
                 ChessPosition target = ConsoleInterface.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                if (capturedPiece != null) {
+                    captured.add(capturedPiece);
+                }
             }
             catch (ChessException e) {
                 System.out.println(e.getMessage());
